6559ec5b6a77b9ee57d4a7974390f485f5f08a89
3louRolTon <skrobat.yura@gmail.com>
2021-05-21 15:44:51 +0300

    fix className

--- magento/app/code/Modules/Banner/Model/ResourceModel/Banner/Collection.php
+++ magento/app/code/Modules/Banner/Model/ResourceModel/Banner/Collection.php
@@ -3,15 +3,13 @@ namespace Modules\Banner\Model\ResourceModel\Banner;
 
 
 use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
-
+use Modules\Banner\Model\Banner;
+use Modules\Banner\Model\ResourceModel\Banner as BannerResource;
 
 class Collection extends AbstractCollection
 {
     protected function _construct()
     {
-        $this->_init(
-            'Modules\Banner\Model\Banner',
-            'Modules\Banner\Model\ResourceModel\Banner'
-        );
+        $this->_init(Banner::class,BannerResource::class);
     }
 }
