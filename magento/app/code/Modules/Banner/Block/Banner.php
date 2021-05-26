aadc993f8333bafafd5eb339b43cd9558e66d881
3louRolTon <skrobat.yura@gmail.com>
2021-05-26 18:11:05 +0300

    add helper config class

--- magento/app/code/Modules/Banner/Block/Banner.php
+++ magento/app/code/Modules/Banner/Block/Banner.php
@@ -1,6 +1,7 @@
 <?php
 namespace Modules\Banner\Block;
 use Magento\Framework\View\Element\Template;
+use Modules\Banner\Model\Dummy;
 
 class Banner extends \Magento\Framework\View\Element\Template
 {
@@ -8,19 +9,22 @@ class Banner extends \Magento\Framework\View\Element\Template
 
     protected $scopeConfig;
 
+    protected $dummy;
+
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
+        Dummy $dummy,
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
     ) {
         $this->scopeConfig = $scopeConfig;
-
+        $this->dummy = $dummy;
         parent::__construct($context);
     }
 
     public function getSettingsBanner()
     {
 
-        return $this->scopeConfig->getValue(self::XML_PATH_EMAIL_RECIPIENT, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        return $this->dummy->getSettings();
 
     }
 }
---
+++ magento/app/code/Modules/Banner/Model/Dummy.php
@@ -0,0 +1,32 @@
+<?php
+namespace Modules\Banner\Model;
+
+class Dummy
+    {
+    /**
+    * @var \Magento\Framework\App\Config\ScopeConfigInterface
+    */
+    protected $scopeConfig;
+
+    /**
+    * Recipient email config path
+    */
+        const XML_PATH_CONFIG = 'banner_section/banner_group/banner_one_show';
+
+    public function __construct(\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig)
+    {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+    * Sample function returning config value
+    **/
+
+    public function getSettings() {
+    $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
+
+    return $this->scopeConfig->getValue(self::XML_PATH_CONFIG, $storeScope);
+
+
+    }
+}
