8259298f3025d0c29efcef496b3e1c63f6ab4377
3louRolTon <skrobat.yura@gmail.com>
2021-05-26 17:52:34 +0300

    fix ajax

--- magento/app/code/Modules/Banner/view/frontend/web/js/modal_overlay.js
+++ magento/app/code/Modules/Banner/view/frontend/web/js/modal_overlay.js
@@ -3,25 +3,17 @@ require([
     "Magento_Ui/js/modal/modal"
 ],function($, modal) {
 
-    var popup;
-    var today = new Date();
-    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-
-    var dateTime = date+' '+time;
-
     $.ajax({
         url:  '/getbanner/banner/index',
         type: 'get',
-        data: {},
         success: function(response){
 
             var banners_id = JSON.parse($.cookie("banners_id"));
             if(banners_id == null) banners_id = [];
+
             var banner = getBanner(response.length, response, banners_id);
 
-            if(banner.banner_one_show && banners_id.indexOf(banner.banner_id) == -1) {
-
+            if(Boolean(Number(banner.banner_one_show)) && banners_id.indexOf(banner.banner_id) == -1) {
                 banners_id.push(banner.banner_id);
                 $.cookie("banners_id", JSON.stringify(banners_id));
             }
@@ -33,29 +25,17 @@ require([
             var options = {
                 type: 'popup',
                 responsive: true,
-                title: banner.banner_title
+                title: banner.banner_name
             };
 
             popup = modal(options, $('#modal-popup'));
             $("#modal").click(function() {
                 $('#modal-popup').modal('openModal');
             });
-        },
-        error: function(){
-
         }
     });
 
     function getBanner(n, banners, banners_id){
-        //console.log(1);
-        //console.log(banners);
-        if(n == 0){
-            var currentBanner = banners[0];
-            if(banners_id.indexOf(currentBanner.banner_id) != -1){
-                return []
-            }
-            return currentBanner;
-        }
         if(n > 0){
             var currentId = Math.floor((Math.random() * n) + 1) - 1;
             var currentBanner = banners[currentId];
